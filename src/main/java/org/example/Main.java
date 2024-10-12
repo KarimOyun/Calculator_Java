1aeb32249941c7db3545047a18b089e184bf72ad
karimoyun <oyun.k@list.ru>
2024-10-13 00:04:13 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        float a = scanner.nextFloat();
+
+        System.out.println("Выберите операцию: ");
+        char operation = scanner.next().charAt(0);
+
+        System.out.println("Введите второе число: ");
+        float b = scanner.nextFloat();
+
+            if (operation == '+') {
+                float result = a + b;
+                System.out.println("Результат: " + a + " + " + b + " = " + result);
+            } else if (operation == '-') {
+                float result = a - b;
+                System.out.println("Результат: " + a + " - " + b + " = " + result);
+            } else if (operation == '*') {
+                float result = a * b;
+                System.out.println("Результат: " + a + " * " + b + " = " + result);
+            } else if (operation == '/') {
+                float result = a / b;
+                System.out.println("Результат: " + a + " / " + b + " = " + result);
+            } else {
+                System.out.println("Операция не поддерживается.");
+            }
+        }
+    }
